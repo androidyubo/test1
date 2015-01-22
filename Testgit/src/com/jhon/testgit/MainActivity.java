1781f19695bb6de21aa68ff5b4d2c35d220e8dd5
jhon <jhon@jhon-PC>
2015-01-22 20:54:08 +0800

    9898

--- Testgit/src/com/jhon/testgit/MainActivity.java
+++ Testgit/src/com/jhon/testgit/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends Activity {
 		System.err.println("I am run 5");
 		System.err.println("I am run 7");
 		System.err.println("I am run 6");
+		System.err.println("I am run 8");
 	}
 
 	@Override
